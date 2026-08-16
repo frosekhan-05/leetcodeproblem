7c43240aa3d8a7a581bf0312c7d0f6248d3098bc
frosekhan-05 <frosekhan05@gmail.com>
2026-08-16 08:37:50 +0530

    Time: 0 ms (100%), Space: 42.2 MB (39.83%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -1,10 +1,12 @@
 class Solution {
     public int xorOperation(int n, int start) {
-        int xor=0;
-        int c=0;
-        for(int i=start;c<n;i+=2){
-            xor^=i;
-            c++;
-        }return xor; 
+
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            res = res ^ (start + 2 * i);
+        }
+
+        return res;
     }
 }
